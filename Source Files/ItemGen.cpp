94ddbbb3712f851e620f519de47e13550b8b341c
Kho2 <43798153+Kho2@users.noreply.github.com>
2019-05-12 22:34:43 -0400

    Update ItemGen.cpp

--- Source Files/ItemGen.cpp
+++ Source Files/ItemGen.cpp
@@ -26,20 +26,20 @@ ItemGen(std::string name, std::string type, int value)
 
 std::string genItemName()
 {
-    std::ifstream inFile_fn {"D:\\itemname.txt"};
+    std::ifstream inFile_fn {"D:\\name.txt"};
     std::ifstream inFile_ln {"D:\\type.txt"};
 
-    std::vector<std::string> itemNames{std::istream_iterator<std::string>{inFile_fn}, {}};
+    std::vector<std::string> name{std::istream_iterator<std::string>{inFile_fn}, {}};
     std::vector<std::string> type{std::istream_iterator<std::string>{inFile_ln}, {}};
 
     auto seed = std::chrono::system_clock::now().time_since_epoch().count();
 
-    std::shuffle(itemNames.begin(), itemNames.end(), std::default_random_engine(seed));
+    std::shuffle(name.begin(), name.end(), std::default_random_engine(seed));
     std::shuffle(type.begin(), type.end(), std::default_random_engine(seed+1));
 
-    for (size_t i = 0; i < std::min(itemNames.size(), type.size()); ++i)
+    for (size_t i = 0; i < std::min(name.size(), type.size()); ++i)
     {
-        std::cout << itemNames[i] << type[i] << "\n";
+        std::cout << name[i] << type[i] << "\n";
     }
 
 }
@@ -47,11 +47,11 @@ std::string genItemName()
 int genItemValue()
 {
     srand((unsigned)time(0)); 
-    int random_integer; 
+    int value; 
     for(int index=0; index<1; index++)
     { 
-        random_integer = (rand()%10)+1; 
-        std::cout << random_integer << std::endl;
+        value = (rand()%10)+1; 
+        std::cout << value << std::endl;
     }
 }
     
