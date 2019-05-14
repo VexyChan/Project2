add1dde9d701fc636c0a0d252cb24e7db4b61dc4
james <james.fenton98@gmail.com>
2019-05-14 01:02:17 -0400

    Vs Code Try 1 [1error]

---
+++ RPG2/Enemy.cpp
@@ -0,0 +1,153 @@
+<<<<<<< 0c0961c5983cb4005218fd73e9963b5aaa004c30:Source Files/Enemy.cpp
+#include "Enemy.h"
+
+Enemy::Enemy()
+{
+}
+
+Enemy::Enemy(std::string name, int health)
+{
+	this->name = name;
+	this->health = health;
+	alive = true;
+	blockValue = 0;
+	item = nullptr;
+}
+
+Enemy::~Enemy()
+{
+}
+
+void Enemy::addToAttacks(std::string name, std::string type, int value)
+{
+	struct Attack tempAttack;
+	tempAttack.name = name;
+	tempAttack.type = type;
+	tempAttack.value = value;
+	attacksVector.push_back(&tempAttack);
+}
+
+std::vector<struct Attack*> Enemy::getAttacks()
+{
+	return attacksVector;
+}
+
+void Enemy::printWarning()
+{
+	for (std::string warn : warning) {
+		std::cout << warn << std::endl;
+	}
+}
+
+void Enemy::setWarning(std::string warning)
+{
+	this->warning.push_back(warning);
+}
+
+Item * Enemy::getItem()
+{
+	return this->item;
+}
+
+void Enemy::setItem(Item * item)
+{
+	this->item = item;
+}
+
+bool Enemy::hasItem()
+{
+	if (item == nullptr) {
+		return false;
+	}
+	else {
+		return true;
+	}
+}
+
+void Enemy::attacked(struct Attack attack)
+{
+	subHealth(attack.value);
+}
+
+
+=======
+#include "Enemy.h"
+
+Enemy::Enemy():Player()
+{
+	name = "";
+	health = 0;
+	alive = true;
+	blockValue = 0;
+	item = nullptr;
+
+}
+
+Enemy::Enemy(std::string name, int health):Player(name)
+{
+	this->name = name;
+	this->health = health;
+	alive = true;
+	blockValue = 0;
+	item = nullptr;
+}
+
+Enemy::~Enemy()
+{
+}
+
+void Enemy::addToAttacks(std::string name, std::string type, int value)
+{
+	struct Attack tempAttack {name, type, value};
+	this->attacksVector.emplace_back(tempAttack);
+}
+
+std::vector<Attack*> Enemy::getAttacks()
+{
+	return attacksVector; 
+	/* the type is wrong// c2440a.cpp
+	struct Base { }; // Defined
+
+	struct Derived; // Forward declaration, not defined
+
+	Base * func(Derived * d) {
+		return static_cast<Base *>(d); // error C2440: 'static_cast' : cannot convert from 'Derived *' to 'Base *'
+	}*/
+}
+
+void Enemy::printWarning()
+{
+	for (std::string warn : warning) {
+		std::cout << warn << std::endl;
+	}
+}
+
+void Enemy::setWarning(std::string warning)
+{
+	this->warning.push_back(warning);
+}
+
+Item * Enemy::getItem()
+{
+	return this->item;
+}
+void Enemy::setItem(Item * item)
+{
+	this->item = item;
+}
+
+bool Enemy::hasItem()
+{
+	if (item == nullptr) {
+		return false;
+	}
+	else {
+		return true;
+	}
+}
+
+void Enemy::attacked(int value) /// there is an issue with the player attacks vs enemy attacks
+{
+	Player::subHealth(value); // this is not an issu
+}
+>>>>>>> Vs Code Try 1 [1error]:RPG2/Enemy.cpp
---
+++ RPG2/Enemy.h
@@ -0,0 +1,140 @@
+<<<<<<< 0c0961c5983cb4005218fd73e9963b5aaa004c30:Header Files/Enemy.h
+#pragma once
+#include <string>
+#include <vector>
+#include <map>
+#include "Item.h"
+#include "Player.h"
+
+class Enemy:public Player
+{
+public:
+	/*
+	CONSTRUCTORS
+	*/
+	Enemy();
+	Enemy(std::string name, int health);
+	~Enemy();
+
+	/*
+	Adds an Attack to the attack vector
+	*/
+	void addToAttacks(std::string name, std::string type, int value);
+	std::vector<struct Attack*> getAttacks();
+
+	/*
+	WARNINGS THAT ENEMY SHOWS WHEN IN A ROOM
+	*/
+	void printWarning();
+	void setWarning(std::string warning);
+
+	/*
+	THE ITEM AN ENEMY IS HOLDING
+	*/
+	Item* getItem();
+	void setItem(Item* item);
+	bool hasItem();
+
+	/*
+	TAKES IN AN ATTACK AND PERFORMS OPERATIONS ON ENEMY HEALTH/STATUS
+	NOT YET COMPLETED
+	*/
+	void attacked(struct Attack attack);
+
+protected:
+
+	std::string name;
+	std::vector<std::string> warning;
+	int health;
+	int blockValue;
+
+	std::vector<struct Attack*> attacksVector;
+
+	bool alive;
+
+	Item* item;
+
+	struct status {
+		std::string statusName;
+		int statusValue;
+		int statusTurns;
+	};
+
+	std::vector<struct status> statusVector;
+};
+=======
+#pragma once
+#include <string>
+#include <vector>
+#include <map>
+#include "Item.h"
+#include "Player.h"
+
+
+
+class Enemy :public Player
+{
+public:
+	/*
+	CONSTRUCTORS
+	*/
+	Enemy();
+	Enemy(std::string name, int health);
+	~Enemy();
+
+
+	/*
+	Adds an Attack to the attack vector
+	*/
+	void addToAttacks(std::string name, std::string type, int value);
+	std::vector<Attack*> getAttacks();
+
+	/*
+	WARNINGS THAT ENEMY SHOWS WHEN IN A ROOM
+	*/
+	void printWarning();
+	void setWarning(std::string warning);
+
+	/*
+	THE ITEM AN ENEMY IS HOLDING
+	*/
+	Item* getItem();
+
+	void setItem(Item* item);
+	bool hasItem();
+	/*
+	TAKES IN AN ATTACK AND PERFORMS OPERATIONS ON ENEMY HEALTH/STATUS
+	NOT YET COMPLETED
+	*/
+	void attacked(int value);
+
+	struct Attack
+	{
+		std::string name;
+		std::string type;
+		int value;
+	};
+
+protected:
+
+	std::string name;
+	std::vector<std::string> warning;
+	int health;
+	int blockValue;
+
+	std::vector<Attack*> attacksVector;
+
+	bool alive;
+
+	Item* item;
+
+	struct status {
+		std::string statusName;
+		int statusValue;
+		int statusTurns;
+	};
+
+	std::vector<status> statusVector;
+};
+
+>>>>>>> Vs Code Try 1 [1error]:RPG2/Enemy.h
