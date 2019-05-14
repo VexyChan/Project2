1f5dd8681dc06ab51fbb61fe367e55c614ea367e
Kho2 <43798153+Kho2@users.noreply.github.com>
2019-05-13 22:03:18 -0400

    Update ItemGen.h

--- Header Files/ItemGen.h
+++ Header Files/ItemGen.h
@@ -8,10 +8,7 @@ public:
     ItemGen();
     ItemGen(std::string name, std::string type, int value);
     ~ItemGen();
-    std::string getName();
-    void setName(std::string name);
-    std::string getType();
-    void setType(std::string type);
+
     void printItem();
     std::string genItemName();
     int genItemValue();
